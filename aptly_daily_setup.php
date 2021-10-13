3bebe528f76332cdef5d4ef8f5f20ae87974dc1e
Robert R. Russell <robert@rrbrussell.com>
2021-10-12 21:12:23 -0500

    Added some prettification tools/linters.

--- aptly_daily_setup.php
+++ aptly_daily_setup.php
@@ -1,11 +1,15 @@
 #!/usr/bin/php
 <?php
 /**
- * These constants define which snapshots are managed by this tool
+ * These constants define which snapshots are managed by this
+ * tool
  */
-const UBUNTU_DISTROS = array("focal", "groovy", "hirsute", "impish");
-const UBUNTU_COMPONENTS = array("main", "multiverse", "restricted", "universe");
-const UBUNTU_PATCH_LEVELS = array("backports", "proposed", "release", "updates");
+const UBUNTU_DISTROS = array("focal", "groovy", "hirsute",
+	"impish");
+const UBUNTU_COMPONENTS = array("main", "multiverse",
+	"restricted", "universe");
+const UBUNTU_PATCH_LEVELS = array("backports", "proposed",
+	"release", "updates");
 const UBUNTU_MIRRORS = array("security","ubuntu");
 const DATES = array("today", "yesterday");
 /*
@@ -13,7 +17,9 @@ $expanded_names = array();
 foreach ($distros as &$distrosvalue) {
 	for( $i = 0; $i < count($subdistro_names); ++$i) {
 		foreach ($components as &$componentsvalue) {
-			$expanded_names[] = sprintf("%s-%s-%s", $distrosvalue, $subdistro_names[$i], $componentsvalue);
+			$expanded_names[] = sprintf("%s-%s-%s",
+			$distrosvalue, $subdistro_names[$i],
+			$componentsvalue);
 		}
 	}
 }
@@ -23,6 +29,9 @@ var_dump($expanded_names);
 
 class OSMirror
 {
+	private const MIRROR_STRING_TEMPLATE = "%s-%s-%s-%s";
+	private const SNAPSHOT_STRING_TEMPLATE =
+		OSMirror::MIRROR_STRING_TEMPLATE . "-%s";
 	public $OS = "";
 	public $Components = array();
 	public $PatchLevels = array();
@@ -31,20 +40,40 @@ class OSMirror
 	/**
 	 * The constructor
 	 */
-	public function __construct(string $os, array $comps, array $patchlevels, array $mirrors) {
+	public function __construct(string $os, array $comps, array
+		$patchlevels, array $mirrors) {
 		$this->OS = $os;
 		$this->Components = $comps;
 		$this->PatchLevels = $patchlevels;
 		$this->Mirrors = $mirrors;
 	}
 
-	public function ConstructArrayOfMirrorsForDate(string $date) {
+	public function ConstructArrayOfSnapshotsForDate(
+		string $date) {
 		$expanded = array();
 		foreach ($this->Mirrors as $mirror) {
 			foreach ($this->PatchLevels as $patchlevel) {
 				foreach ($this->Components as $component) {
-					$expanded[] = sprintf("%s-%s-%s-%s-%s", $mirror,
-						$this->OS, $patchlevel, $component, $date);
+					$expanded[] = 
+					sprintf(OSMirror::SNAPSHOT_STRING_TEMPLATE,
+						$mirror, $this->OS, $patchlevel,
+						$component, $date);
+				}
+			}	
+		}
+		unset($mirror, $patchlevel, $component);
+		return $expanded;
+	}
+
+	public function ConstructArrayOfMirrors() {
+		$expanded = array();
+		foreach ($this->Mirrors as $mirror) {
+			foreach ($this->PatchLevels as $patchlevel) {
+				foreach ($this->Components as $component) {
+					$expanded[] = 
+					sprintf(OSMirror::MIRROR_STRING_TEMPLATE,
+						$mirror, $this->OS, $patchlevel,
+						$component);
 				}
 			}	
 		}
@@ -58,10 +87,12 @@ class OSMirror
  */
 $ubuntu_os_mirrors = array();
 foreach(UBUNTU_DISTROS as $distro) {
-	$ubuntu_os_mirrors[] = new OSMirror($distro, UBUNTU_COMPONENTS, UBUNTU_PATCH_LEVELS, UBUNTU_MIRRORS);
+	$ubuntu_os_mirrors[] = new OSMirror($distro,
+	UBUNTU_COMPONENTS, UBUNTU_PATCH_LEVELS, UBUNTU_MIRRORS);
 }
 
-var_dump($ubuntu_os_mirrors[0]->ConstructArrayOfMirrorsForDate("today"));
+var_dump($ubuntu_os_mirrors[0]->
+	ConstructArrayOfSnapshotsForDate("today"));
 
 //exec("aptly snapshot list -raw", $all_snapshots );
 
@@ -72,6 +103,7 @@ foreach($all_snapshots as $snapshot) {
 }
 */
 
-$aptly_merge_commandline = "aptly snapshot merge -latest result sources"
+$aptly_merge_commandline =
+"aptly snapshot merge -latest result sources"
 
 ?>
